8ebefdb3af9aed9661fbbaf87676fcfd769ef049
Ullas M <ullas9080@gmail.com>
2024-08-18 22:22:01 +0530

    File added

---
+++ files/FileHandling.java
@@ -0,0 +1,19 @@
+package DATASTRUCTURE.files;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandling {
+
+public static void main(String[] args) throws IOException {
+   
+File file=new File("myfile.txt");
+
+FileWriter fw =new FileWriter(file);
+
+
+
+}
+}
